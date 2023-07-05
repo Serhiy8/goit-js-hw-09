708d1ed04f612bae7464a634f5eb4aad02738933
Serhiy8 <markovsm86@gmail.com>
2023-07-05 21:20:48 +0300

    task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+const refs = {
+  body: document.querySelector('body'),
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
+
+refs.startBtn.addEventListener('click', onStartPush);
+refs.stopBtn.addEventListener('click', onStopPush);
+
+let interval;
+
+function changeBodyColor() {
+  refs.body.style.backgroundColor = getRandomHexColor();
+}
+
+function onStartPush() {
+  refs.startBtn.setAttribute('disabled', '');
+  interval = setInterval(changeBodyColor, 1000);
+}
+
+function onStopPush() {
+  refs.startBtn.removeAttribute('disabled');
+  clearInterval(interval);
+}
