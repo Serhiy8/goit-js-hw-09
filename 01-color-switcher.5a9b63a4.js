ab1d6f4058493b4e1ccb031ece89111b5af4714a
Serhiy8 <125665311+Serhiy8@users.noreply.github.com>
2023-07-06 17:57:06 +0000

    Deploying to gh-pages from @ Serhiy8/goit-js-hw-09@76a1cee254096deb9b8c360a0a1257736b6da30f 🚀

--- 01-color-switcher.5a9b63a4.js
+++
@@ -1,2 +0,0 @@
-const t={body:document.querySelector("body"),startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]")};t.startBtn.addEventListener("click",(function(){t.startBtn.setAttribute("disabled",""),e=setInterval(n,1e3)})),t.stopBtn.addEventListener("click",(function(){t.startBtn.removeAttribute("disabled"),clearInterval(e)}));let e=null;function n(){t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}
-//# sourceMappingURL=01-color-switcher.5a9b63a4.js.map
